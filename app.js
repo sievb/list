da0e39735b16fa153be0ff5d02ed607b834324aa
sievb <38827920+sievb@users.noreply.github.com>
2020-05-29 23:57:26 -0700

    Add complete list files

---
+++ app.js
@@ -0,0 +1,87 @@
+$(function () {
+  initTable();
+});
+
+var $table = $("#itemList");
+var $button = $("#removeBtn");
+
+function initTable() {
+  $table.bootstrapTable("destroy").bootstrapTable({
+    columns: [
+      [
+        {
+          field: "state",
+          checkbox: true,
+          align: "center",
+          valign: "middle",
+        },
+        {
+          title: "Location",
+          field: "location",
+          align: "center",
+          valign: "middle",
+        },
+        {
+          title: "Item",
+          field: "item",
+          align: "center",
+        },
+        {
+          title: "Quantity",
+          field: "quantity",
+          align: "center",
+        },
+      ],
+    ],
+  });
+}
+
+$("input").keydown(function (e) {
+  if (e.keyCode == 13) {
+    createRow();
+  }
+});
+
+$("#addBtn").click(function () {
+  createRow();
+});
+
+function createRow() {
+  let location = $("#location").val();
+  let item = $("#item").val();
+  let quantity = $("#quantity").val();
+  $table = $("#itemList");
+
+  $table.bootstrapTable("append", [
+    {
+      location: location,
+      item: item,
+      quantity: quantity,
+    },
+  ]);
+
+  clearInputs();
+  $("#location").focus();
+  return false;
+}
+
+function clearInputs() {
+  $("#location").val("");
+  $("#item").val("");
+  $("#quantity").val("");
+}
+
+$("#removeBtn").click(function () {
+  var ids = $.map($table.bootstrapTable("getSelections"), function (row) {
+    return row.location;
+  });
+
+  $table.bootstrapTable("remove", {
+    field: "location",
+    values: ids,
+  });
+});
+
+$("#clearBtn").click(function () {
+  $table.bootstrapTable("removeAll");
+});
